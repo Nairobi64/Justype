650ab248497d6d00aebf4ef10e870928d6bf7860
Nairobi64 <110598402+Nairobi64@users.noreply.github.com>
2023-09-29 02:29:45 +0000

    petite modification des mots et phrases

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,3 @@
 // Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
-const listeMots = ["Cachalot", "Pétunia", "Serviette"]
-const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+const listeMots = ["Montagne", "Ferrari", "Mandela"]
+const listePhrases = ["Vivons heureux !", "La vie, le sport et le reste", "Merci pour le tchep !"]
